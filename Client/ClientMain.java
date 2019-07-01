5490a6d6725810036879749a97050786cfd890d7
Michail Danousis <mikedan171@gmail.com>
2019-07-01 15:20:53 +0300

    Add files via upload

---
+++ Client/ClientMain.java
@@ -0,0 +1,16 @@
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+//Starts a chat client
+public class ClientMain {
+
+    
+    public static void main(String[] args) {
+        ChatFrame frame=new ChatFrame();
+    }
+    
+}
